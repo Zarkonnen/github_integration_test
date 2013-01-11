5f5329b04b20d24ae4d93b3158bcefd2e8ca4856
David Stark <david.stark@zarkonnen.com>
2013-01-11 08:45:26 -0800

    Committed wef.cs.

---
+++ wef.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using NUnit.Framework;
+using Selenium;
+
+namespace SeleniumTests
+{
+	[TestFixture]
+	public class Untitled
+	{
+		private ISelenium selenium;
+		private StringBuilder verificationErrors;
+		
+		[SetUp]
+		public void SetupTest()
+		{
+			selenium = new DefaultSelenium("localhost", 4444, "*chrome", "http://sebuilder.github.com");
+			selenium.Start();
+			verificationErrors = new StringBuilder();
+		}
+		
+		[TearDown]
+		public void TeardownTest()
+		{
+			try
+			{
+				selenium.Stop();
+			}
+			catch (Exception)
+			{
+				// Ignore errors if unable to close the browser
+			}
+			Assert.AreEqual("", verificationErrors.ToString());
+		}
+		
+		[Test]
+		public void TheUntitledTest()
+		{
+			selenium.Open("/se-builder/");
+			selenium.WaitForPageToLoad("60000");
+		}
+	}
+}
