02dbf565f68dea48306b8d662770ea312e8ce1ec
David Stark <david.stark@zarkonnen.com>
2013-01-11 08:40:51 -0800

    Committed oijoij.pl.

---
+++ oijoij.pl
@@ -0,0 +1,14 @@
+use strict;
+use warnings;
+use Time::HiRes qw(sleep);
+use Test::WWW::Selenium;
+use Test::More "no_plan";
+use Test::Exception;
+
+my $sel = Test::WWW::Selenium->new( host => "localhost", 
+                                    port => 4444, 
+                                    browser => "*chrome", 
+                                    browser_url => "http://sebuilder.github.com" );
+
+$sel->open_ok("/se-builder/");
+$sel->wait_for_page_to_load_ok("60000");
