5606262894f378d35ce7a5599a5ba8857335860f
David Stark <david.stark@zarkonnen.com>
2013-01-14 09:24:46 -0800

    Committed hamcat.rb.

--- hamcat.rb
+++ hamcat.rb
@@ -1,29 +1,6 @@
-require "test/unit"
-require "rubygems"
-gem "selenium-client"
-require "selenium/client"
+require 'rubygems'
+require 'selenium-webdriver'
+wd = Selenium::WebDriver.for :firefox
 
-class Untitled < Test::Unit::TestCase
-
-  def setup
-    @verification_errors = []
-    @selenium = Selenium::Client::Driver.new \
-      :host => "localhost",
-      :port => 4444,
-      :browser => "*chrome",
-      :url => "http://sebuilder.github.com",
-      :timeout_in_second => 60
-
-    @selenium.start_new_browser_session
-  end
-  
-  def teardown
-    @selenium.close_current_browser_session
-    assert_equal [], @verification_errors
-  end
-  
-  def test_untitled
-    @selenium.open "/se-builder/"
-    @selenium.wait_for_page_to_load "50000"
-  end
-end
+wd.get "http://sebuilder.github.com/se-builder/"
+wd.quit
