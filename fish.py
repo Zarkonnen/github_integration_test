311f0210771a9fdabe1c6ec8a7576361d28ade2f
David Stark <david.stark@zarkonnen.com>
2013-02-13 09:04:55 -0800

    Committed fish.py.

---
+++ fish.py
@@ -0,0 +1,21 @@
+from selenium.webdriver.firefox.webdriver import WebDriver
+from selenium.webdriver.common.action_chains import ActionChains
+import time
+
+success = True
+wd = WebDriver()
+wd.implicitly_wait(60)
+
+def is_alert_present(wd):
+    try:
+        wd.switch_to_alert().text
+        return True
+    except:
+        return False
+
+try:
+    wd.get("http://sebuilder.github.com/se-builder/")
+finally:
+    wd.quit()
+    if not success:
+        raise Exception("Test failed.")
