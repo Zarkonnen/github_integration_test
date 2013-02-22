fcf6b2b5908bb24912cd16f130949fe88a481509
David Stark <david.stark@zarkonnen.com>
2013-02-22 09:26:34 -0800

    Committed EEE.java.

---
+++ EEE.java
@@ -0,0 +1,16 @@
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+public class EEE {
+
+	public static Test suite() {
+		TestSuite suite = new TestSuite();
+		suite.addTestSuite(_B_.class);
+		suite.addTestSuite(_B_.class);
+		return suite;
+	}
+
+	public static void main(String[] args) {
+		junit.textui.TestRunner.run(suite());
+	}
+}
