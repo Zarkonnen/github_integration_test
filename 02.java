5d0c259bf94abd27ef0b1fef864dbfc7e4383d98
David Stark <david.stark@zarkonnen.com>
2013-02-23 04:26:02 -0800

    Committed 02.java.

---
+++ 02.java
@@ -0,0 +1,30 @@
+import java.util.concurrent.TimeUnit;
+import java.util.Date;
+import java.io.File;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.*;
+import static org.openqa.selenium.OutputType.*;
+
+public class 02 {
+    public static void main(String[] args) throws Exception {
+        FirefoxDriver wd;
+        wd = new FirefoxDriver();
+        wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+        wd.get("http://www.metafilter.com/");
+        if (!wd.findElement(By.tagName("html")).getText().contains("Bob Godfrey 1921-2013")) {
+            System.out.println("verifyTextPresent failed");
+        }
+        wd.close();
+    }
+    
+    public static boolean isAlertPresent(FirefoxDriver wd) {
+        try {
+            wd.switchTo().alert();
+            return true;
+        } catch (NoAlertPresentException e) {
+            return false;
+        }
+    }
+}
