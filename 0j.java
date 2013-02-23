d5b27989c3948a92260a63f8bd12c5ef73447224
David Stark <david.stark@zarkonnen.com>
2013-02-23 04:24:28 -0800

    Committed 0j.java.

---
+++ 0j.java
@@ -0,0 +1,15 @@
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+public class 0j {
+
+	public static Test suite() {
+		TestSuite suite = new TestSuite();
+		suite.addTestSuite(-2.class);
+		return suite;
+	}
+
+	public static void main(String[] args) {
+		junit.textui.TestRunner.run(suite());
+	}
+}
