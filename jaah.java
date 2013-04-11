e09ce4d4cc9c1f24b963a3fca1c601778f1150b6
David Stark <david.stark@zarkonnen.com>
2013-04-11 02:19:05 -0700

    Committed jaah.java.

--- jaah.java
+++ jaah.java
@@ -3,11 +3,11 @@ package com.example.tests;
 import com.thoughtworks.selenium.*;
 import java.util.regex.Pattern;
 
-public class wfe extends SeleneseTestCase {
+public class jaah extends SeleneseTestCase {
 	public void setUp() throws Exception {
 		setUp("http://www.metafilter.com", "*chrome");
 	}
-	public void testWfe() throws Exception {
+	public void testJaah() throws Exception {
 		selenium.open("/");
 		selenium.waitForPageToLoad("aa");
 	}
