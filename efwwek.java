8f5d83a9898ae7a83fc6a88d3d69f8887724c0c8
David Stark <david.stark@zarkonnen.com>
2013-04-25 13:58:01 -0700

    Committed efwwek.java.

---
+++ efwwek.java
@@ -0,0 +1,17 @@
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+public class Efwwek {
+
+	public static Test suite() {
+		TestSuite suite = new TestSuite();
+		suite.addTestSuite(efwfewg.class);
+		suite.addTestSuite(ewfwfei.class);
+		suite.addTestSuite(ewffwefwe.class);
+		return suite;
+	}
+
+	public static void main(String[] args) {
+		junit.textui.TestRunner.run(suite());
+	}
+}
